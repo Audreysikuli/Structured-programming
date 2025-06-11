3a57a445f6c86375fc7b3849959037502d4439ab
Christel Sikuli <sikuliaudrey@gmail.com>
2025-06-11 15:59:11 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+   ## Assignment 1: Differentiate Between Assigning
+
+   ## value
+   
+## data type
+"""
+intergers
+floats
+strings/characters
+Boolean
+"""
+
+number_one = 30 #int
+number_two = 3.17 # float
+
+last_name = "Sikuli" # string
+first_name = 'Audrey'
+is_attentive = False
+is_present = True
+
+## checking for the datatype
+print(type(number_one))
+print(type(is_attentive))
+
+## conversions
+new_number_two = int(number_two)
+print(f"integer: {new_number_two} datatype: {type(new_number_two)}")
+
+### Assignment 
