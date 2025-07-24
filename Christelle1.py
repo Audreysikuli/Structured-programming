e2094ce2a0bba79448ee6a3fa9065e4c7eee285e
Christel Sikuli <sikuliaudrey@gmail.com>
2025-07-24 14:55:26 +0300

    Add files via upload

---
+++ Christelle1.py
@@ -0,0 +1,12 @@
+# Ask user for length and width
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+# Calculate area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Display the results
+print("Area:", area)
+print("Perimeter:", perimeter)
+
